a3046e1f973ba5c9edb0f311bb04a6d94ce78c72
Andre Luiz Moura Borges <andreluizmb82@gmail.com>
2024-10-08 21:13:04 -0300

    feat(solution_1.js): implement challenge class
    This commit adds the class implementation requested in the challenge in a simple and objective way.

---
+++ js/src/solution_1.mjs
@@ -0,0 +1,30 @@
+const TIPO = ["mago", "guerreiro", "monge", "ninja"]
+export default class Heroi {
+  constructor(nome, idadde, tipo = "mago") {
+    this.nome = nome
+    this.idade = idadde
+    this.tipo = tipo
+  }
+
+  atacar() {
+    let ataque = ""
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia"
+        break
+      case "guerreiro":
+        ataque = "espada"
+        break
+      case "monge":
+        ataque = "artes marciais"
+        break
+      case "ninja":
+        ataque = "shuriken"
+        break
+      default:
+        ataque = "ataque desconhecido"
+        break
+    }
+    console.log(`O ${this.tipo} atacou usando ${ataque}`)
+  }
+}
