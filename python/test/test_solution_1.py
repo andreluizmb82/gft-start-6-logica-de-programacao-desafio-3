e25b358b292b1d0fa9e4708e7c051c7f8009ab72
Andre Luiz Moura Borges <andreluizmb82@gmail.com>
2024-10-09 21:36:45 -0300

    test(test_solution_1.py): fix test logic
    Correct the test logic in test_solution_1.py to properly
    
    validate outputs and improve consistency de Hero.py.

--- python/test/test_solution_1.py
+++ python/test/test_solution_1.py
@@ -1,6 +1,17 @@
 from Test import Test
+import sys
+import os
+# Adiciona a pasta "src" ao caminho do sistema
+src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.append(src_path)
 
-Test.assert_equals("oi","ola" ,"Testa se oi é igual a ola")
+from module.Hero import Hero
 
-Test.assert_equals("oi","ola" ,"Testa se oi é igual a ola")
+
+
+
+Test.assert_equals("O mago atacou usando magia", Hero('Gandalf', 1000, 'mago').attack(),"1º - testa se a instancia da class Hero responde corretamente quando executado o método attack")
+Test.assert_equals("O guerreiro atacou usando espada", Hero("Kenshin Himura", 28, 'guerreiro').attack(), "2º - testa se a instancia da class Hero responde corretamente quando executado o método attack")
+Test.assert_equals("O monge atacou usando artes marciais", Hero('Tenzin', 51, 'monge').attack(), "3º - testa se a instancia da class Hero responde corretamente quando executado o método attack")
+Test.assert_equals("O ninja atacou usando shuriken", Hero('Kakashi', 30, 'ninja').attack(), "4º - testa se a instancia da class Hero responde corretamente quando executado o método attack")
 Test.print_summary()
